2f656a1dae8e530f1d5070e221897551d6825668
Pablo Rivera <pabloriverar.sv@gmail.com>
2020-08-25 09:17:50 -0600

    primera configuracion php

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php
+//ip de la pc servidor
+define("DB_HOST","localhost");
+
+//Nombre de la base de datos
+define("DB_NAME","dbsistema");
+
+//Usuario de la base de datos
+define("DB_USERNAME", "root");
+
+//Contrasena del usuario de la base de datos
+define("DB_PASSWORD","");
+
+//definimos la codificacion de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","ITVentas");
+ ?>
